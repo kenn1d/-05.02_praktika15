00c9e308b8ef3aa5051d7820a72ba821042a3d76
kenn1d <makarkibanov220@gmail.com>
2025-12-16 22:28:19 +0500

    Загрузить изменения в систему контроля версий, указать комментарий «Реализованные модели. Кибанов

---
+++ Models/Document.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+
+namespace praktika15.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Src { get; set; }
+        public string Name { get; set; }
+        public string User {  get; set; }
+        public int IdDocument { get; set; }
+        public DateTime Date { get; set; }
+        public int Status { get; set; }
+        public int Direction { get; set; }
+    }
+}
